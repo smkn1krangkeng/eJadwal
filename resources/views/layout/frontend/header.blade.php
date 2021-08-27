d12a342d43e8a8b530f29ed80d7ac0288f7e3c09
ozonerik <ozonerik@gmail.com>
2021-08-27 21:39:26 +0700

    favicon frontend

--- resources/views/layout/frontend/header.blade.php
+++ resources/views/layout/frontend/header.blade.php
@@ -6,3 +6,8 @@
   <link rel="stylesheet" href="{{url('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{url('dist/css/adminlte.min.css')}}">
+  <!-- favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="{{url('apple-touch-icon.png')}}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{url('favicon-32x32.png')}}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{url('favicon-16x16.png')}}">
+  <link rel="manifest" href="{{url('site.webmanifest')}}">
